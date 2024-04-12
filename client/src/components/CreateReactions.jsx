db8b941c492a22310920b4139f49dc95ab757120
Joshua Ombasa <nyambegaombasa@gmail.com>
2024-04-12 23:53:33 +0300

    initial commit

--- client/src/components/CreateReactions.jsx
+++ client/src/components/CreateReactions.jsx
@@ -1,11 +1,13 @@
 import { useState } from "react"
+import axios from "axios"
 
-
-export default function CreateReaction() {
+export default function CreateReaction({tweetId}) {
     const [reaction,setReaction] = useState('')
 
     async function handleSubmit(event) {
         event.preventDefault()
+        const res = await axios.post(`http://localhost:4016/tweets/${tweetId}/reactions`, { reaction })
+        setReaction('')
     }
 
     return (
