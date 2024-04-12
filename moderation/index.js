4a95ca9c1364121a0edcffbf7d12e1cb417974f1
Joshua Ombasa <nyambegaombasa@gmail.com>
2024-04-12 23:52:35 +0300

    initial commit

--- moderation/index.js
+++ moderation/index.js
@@ -2,10 +2,32 @@ const express = require('express')
 const cors = require('cors')
 const axios = require('axios')
 const app = express()
-const PORT = 4015
+const PORT = 4018
 
-app.post('/events', (request, response) => {
+app.use(express.json())
 
+app.post('/events', async (request, response) => {
+
+    console.log(request.body)
+    const event = request.body
+    const { type, data } = event
+
+    try {
+        if (type === 'ReactionCreated') {
+            const { id, reaction, tweetId, status } = data
+            const statusValue = reaction.includes('sex') ? 'rejected' : 'approved'
+
+            await axios.post(`http://localhost:4020/events`,
+            {
+                type: 'ReactionModerated',
+                data: { id, reaction, tweetId, status: statusValue }
+            })
+        }
+
+        response.send({})
+    } catch (error) {
+
+    }
 })
 
 app.get('', (request, response) => {
