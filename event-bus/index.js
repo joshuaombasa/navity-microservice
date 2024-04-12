041f48546274e8531fa0e64ea5ad1755b2aa5300
Joshua Ombasa <nyambegaombasa@gmail.com>
2024-04-12 23:53:09 +0300

    initial commit

--- event-bus/index.js
+++ event-bus/index.js
@@ -2,16 +2,35 @@ const express = require('express')
 const cors = require('cors')
 const axios = require('axios')
 const app = express()
-const PORT = 4015
+const PORT = 4020
 
-app.post('/events', (request, response) => {
+const events = []
 
+app.use(cors())
+app.use(express.json())
+
+app.post('/events', async (request, response) => {
+    const event = request.body
+    console.log(event)
+    events.push(event)
+
+    try {
+        await axios.post(`http://localhost:4015/events`, event)
+        await axios.post(`http://localhost:4016/events`,event)
+        await axios.post(`http://localhost:4018/events`,event)
+        await axios.post(`http://localhost:4017/events`,event)
+
+
+        response.json({status: 'Ok'})
+    } catch (error) {
+        console.log(error.name)
+    }
 })
 
-app.get('', (request, response) => {
-
+app.get('/events', (request, response) => {
+    response.send(events)
 })
 
 app.listen(PORT, () => {
-    console.log(`server runnin on port ${PORT}`)
+    console.log(`server running on port ${PORT}`)
 })
